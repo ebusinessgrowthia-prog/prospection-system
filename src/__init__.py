0b79dab8b2fcd86f980996a2491f94061335d1cb
ebusinessgrowthia-prog <ebusinessgrowthia@gmail.com>
2025-09-03 13:27:09 +0100

    src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+EBUSINESS AI - Automatisation de Prospection
+Système complet de prospection automatisée pour e-commerçants en Europe francophone
+"""
+
+__version__ = "1.0.0"
+__author__ = "Geraldo Domingo"
+__email__ = "ia.ebusinessag@gmail.com"
